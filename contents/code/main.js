4c1744d3e4a4edfd64a80f84ea104705e52a734d
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-06-29 17:55:00 -0400

    Register shortcut to toggle kitty window
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,28 +1,35 @@
-
-const clients = workspace.clientList();
-
-let client = null;
-
-for (var i = 0; i < clients.length; i++) {
-  let client_caption = clients[i].caption;
-
-  if (client_caption.startsWith(":KTKWIN:"))
-  {
-    print("Found");
-    client = clients[i];
-    break;
-  }
-}
-
-if (client)
+function init()
 {
-  if (client.minimized)
-  {
-    client.setMaximize(true, true);
-    client.keepAbove = true;
-    client.keepBelow = false;
-    workspace.activeClient = client;
-  }else{
-    client.minimized = true;
-  }
+    const clients = workspace.clientList();
+
+    var client = null;
+
+    for (var i = 0; i < clients.length; i++) {
+        let client_caption = clients[i].caption;
+
+        if (client_caption.startsWith(":KTKWIN:"))
+        {
+            client = clients[i];
+            break;
+        }
+    }
+
+    function toggleClient() {
+        if (client)
+        {
+            if (client.minimized)
+            {
+                client.setMaximize(true, true);
+                client.keepAbove = true;
+                client.keepBelow = false;
+                workspace.activeClient = client;
+            }else{
+                client.minimized = true;
+            }
+        }
+    }
+
+    registerShortcut("ToggleKittyWindow", "Toggle Kitty window", "F12", toggleClient);
 }
+
+init();
